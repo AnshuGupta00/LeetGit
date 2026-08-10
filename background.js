9e4097855548e805d5e6d31b0b1442da40897245
Anshu Gupta <anshu.gupta351613@gmail.com>
2026-08-10 18:55:27 +0530

    done

--- background.js
+++ background.js
@@ -85,7 +85,7 @@ async function handlePostToLinkedInAPI(text) {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${linkedinToken}`,
-        'LinkedIn-Version': '202506',
+        'LinkedIn-Version': '202608',
         'X-Restli-Protocol-Version': '2.0.0',
         'Content-Type': 'application/json'
       },
